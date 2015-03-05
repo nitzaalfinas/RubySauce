fc3cebeb1065e7afdb62e89a1b6015a46e1484f9
nitzaalfinas <nitzaalfinas@gmail.com>
2015-03-05 10:59:49 +0700

    modified:   db/migrate/20141230120303_create_articles.rb

--- db/migrate/20141230120303_create_articles.rb
+++ db/migrate/20141230120303_create_articles.rb
@@ -1,10 +1,27 @@
 class CreateArticles < ActiveRecord::Migration
-  def change
-    create_table :articles do |t|
-      t.string :title
-      t.text :body
+	def up
+		self.connection.execute %Q( 
+			CREATE TABLE IF NOT EXISTS articles (
+				id int(11) NOT NULL AUTO_INCREMENT,
+				title varchar(255) DEFAULT NULL,
+				body text,
+			  	author_id int(11) DEFAULT NULL,
+			  	permalink varchar(255) DEFAULT NULL,
+			  	excerp text,
+			  	feat_img varchar(255) DEFAULT NULL,
+			  	comment_status varchar(3) DEFAULT NULL COMMENT 'yes or no',
+			  	publish_status varchar(20) DEFAULT NULL COMMENT 'draft or publish',
+			  	publish_visibility varchar(10) DEFAULT NULL COMMENT 'public or private',
+			  	article_type varchar(10) DEFAULT NULL COMMENT 'article or page',
+			  	article_vcount int(11) DEFAULT NULL COMMENT 'article view count',
+			  	created_at datetime NOT NULL,
+			  	updated_at datetime NOT NULL,
+			  	PRIMARY KEY (id)
+			);
+	 	)
+	end
 
-      t.timestamps null: false
-    end
-  end
+	def down
+		self.connection.execute "DROP TABLE IF EXISTS articles;"
+	end
 end
