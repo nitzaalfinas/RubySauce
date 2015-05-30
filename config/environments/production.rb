39b9c202a7a0b7d5493220f35d9dad59bbb6e8bf
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-05-30 22:15:46 +0700

    mail configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => 'diasporaminang.com'}
+  config.action_mailer.default_url_options = {:host => 'ror_cms.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "127.0.0.1",
     :port    => 25,
-    :domain  => 'diasporaminang.com'
+    :domain  => 'ror_cms.com'
   }
 
 end
