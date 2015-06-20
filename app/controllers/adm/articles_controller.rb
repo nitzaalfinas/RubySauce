e75ea125cc8a2bd1e0745f6eee02130ab40e1f1a
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-06-20 13:50:51 +0700

    searching for title and content on admin page

--- app/controllers/adm/articles_controller.rb
+++ app/controllers/adm/articles_controller.rb
@@ -7,7 +7,20 @@ class Adm::ArticlesController < ApplicationController
 	layout "adm_layout"
 
 	def index
-		@articles = VArticle.all.order('created_at DESC').paginate(page: params[:page], per_page: 10)
+		@q = params[:q]
+    
+    if @q
+      @q.gsub!(/[^0-9a-z ]/i, '')
+    
+      @arr = @q.split(" ")
+
+      @qa = ""
+      @arr.each { |q| @qa = @qa+"article_all like '%"+q+"%' and "; }
+      
+      @articles = VArticle.where(@qa[0..(@qa.length - 5)]).order('created_at DESC').paginate(page: params[:page], per_page: 10)
+    else
+      @articles = VArticle.all.order('created_at DESC').paginate(page: params[:page], per_page: 10)
+    end
 	end #index
 
 	def new
