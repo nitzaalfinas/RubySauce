a028833d3df85758846bc0d6adef5b2b83201a94
Nitza Alfinas <itsme@nitzaalfinas.com>
2015-08-19 07:28:26 +0700

    perbaikan tampilan

--- app/controllers/adm/my_profile_controller.rb
+++ app/controllers/adm/my_profile_controller.rb
@@ -23,12 +23,10 @@ class Adm::MyProfileController < ApplicationController
     @full_name = params[:user][:full_name]
     @nick_name = params[:user][:nick_name]
     
-    
     @user = User.find(current_user.id)
     @user.full_name = @full_name
     @user.nick_name = @nick_name
     @user.save
-    
     
     if(params[:user][:avatar_file])
       if(@user.avatar_file)
@@ -49,7 +47,6 @@ class Adm::MyProfileController < ApplicationController
       end
       
       
-      
       #upload original
       File.open(Rails.root.join('public/images/original',params[:user][:avatar_file].original_filename),'wb') do |file|
         file.write(params[:user][:avatar_file].read)
@@ -59,17 +56,25 @@ class Adm::MyProfileController < ApplicationController
       randomx = Random.new
       random_id = randomx.rand(1000000...100000000).to_s
       file_extension = File.extname(Rails.root.to_s+'/public/images/original/'+params[:user][:avatar_file].original_filename)
-      new_file_name = 'avatar-'+random_id+file_extension
+      new_file_name = 'avatar-'+current_user.id.to_s+'-'+random_id+file_extension
       
       # rename
       File.rename(Rails.root.to_s+'/public/images/original/'+params[:user][:avatar_file].original_filename, Rails.root.to_s+'/public/images/original/'+new_file_name)
       
+      
+      # thumb
+      FileUtils.cp(Rails.root.to_s+'/public/images/original/'+new_file_name, Rails.root.to_s+'/public/images/thumb/'+new_file_name)
+      system("convert "+Rails.root.to_s+"/public/images/thumb/"+new_file_name+" -resize '250x200^' "+Rails.root.to_s+"/public/images/thumb/"+new_file_name)
+      
+      # show
+      FileUtils.cp(Rails.root.to_s+'/public/images/original/'+new_file_name, Rails.root.to_s+'/public/images/show/'+new_file_name)
+      system("convert "+Rails.root.to_s+"/public/images/show/"+new_file_name+" -resize '800x600^' "+Rails.root.to_s+"/public/images/show/"+new_file_name)
+    
       # update database with new file
       @user.avatar_file = new_file_name
       @user.save
       
-      # copy ke thumb dan show
-      
+      redirect_to('/adm/my_profile')
       
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -84,3 +84,8 @@ Rails.application.routes.draw do
   end
  
 end
+
+
+#/**
+#Membuat halaman profile
+#*/
