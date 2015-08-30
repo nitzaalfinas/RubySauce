d112ac8e9fd5aa3404e72e3b29ee002390d3c231
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-08-30 23:01:55 +0700

    modified:   app/helpers/adm/media_helper.rb 	modified:   db/migrate/20150614015305_create_themes.rb

--- app/helpers/adm/media_helper.rb
+++ app/helpers/adm/media_helper.rb
@@ -2,7 +2,7 @@ module Adm::MediaHelper
   
   def show_image(image_file_name)
     if(File.exists?(Rails.root.to_s+'/public/images/thumb/'+image_file_name))
-      image_tag('thumb/'+med.med_storage_name, alt: med.med_alt, style: "margin-bottom: 10px; max-width: 250px;")
+      image_tag('thumb/'+image_file_name, alt: image_file_name, style: "margin-bottom: 10px; max-width: 250px;")
     else
       image_tag('thumb/default.png', style: "margin-bottom: 10px; max-width: 250px;")
     end
--- db/migrate/20150614015305_create_themes.rb
+++ db/migrate/20150614015305_create_themes.rb
@@ -1,7 +1,7 @@
 class CreateThemes < ActiveRecord::Migration
   def change
     create_table :themes do |t|
-      t.string :name
+      t.string :folder_name
       t.integer :active, :default => 0
 
       t.timestamps null: false
