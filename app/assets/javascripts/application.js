9cd9ea5e88b385fa0e3dc7a607efe2912229c48c
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-08-30 09:11:35 +0700

    default theme to simple theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 // require_tree .  // REQUIRE_TREE DISABLED!!!!!!!!!!!!!!!!!!!!!
+//= require clean-blog.min
 //= require article_single
 //= require discuss
 //= require galleries
 //= require landing
 //= require page_single
 //= require percategory
-//= require search
+
+
+
+//template ini adalah template ror_cms_2
--- app/helpers/article_page_helper.rb
+++ app/helpers/article_page_helper.rb
@@ -1,4 +1,2 @@
 module ArticlePageHelper
-  
-  
 end
