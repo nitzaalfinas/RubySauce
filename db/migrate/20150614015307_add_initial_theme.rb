3c22116b84adcc5d86590fc65a40685b0e5fe01b
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-09-01 11:26:04 +0700

    modified:   db/migrate/20150614015307_add_initial_theme.rb 	modified:   public/images/themes/.gitignore

--- db/migrate/20150614015307_add_initial_theme.rb
+++ db/migrate/20150614015307_add_initial_theme.rb
@@ -1,16 +1,9 @@
 class AddInitialTheme < ActiveRecord::Migration
-  def up
-    
+  def up   
     self.connection.execute %Q( 
       INSERT INTO `themes` (`id`, `folder_name`, `active`, `created_at`, `updated_at`) VALUES
-      (1, 'ror_cms', 0, '0000-00-00 00:00:00', '2015-07-04 07:07:39'),
-      (2, 'ror_cms_2', 1, '0000-00-00 00:00:00', '2015-08-30 02:04:04'),
-      (3, 'super_simple', 0, '0000-00-00 00:00:00', '2015-07-04 07:04:53'),
-      (4, 'super_simple_d', 0, '2015-07-04 07:07:31', '2015-08-30 02:04:03');
+      (1, 'clean_blog', 0, '0000-00-00 00:00:00', '2015-07-04 07:07:39'),
+      (2, 'default', 1, '0000-00-00 00:00:00', '2015-08-30 02:04:04');
 	 	)
-  end
- 
-  def down
-    User.delete_all
   end
 end
