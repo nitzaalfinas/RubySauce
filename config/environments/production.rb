ca108b8573010e1e96bce5d881c127870ea0da7d
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-02-10 19:14:05 +0700

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => 'rubysauce.org'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => 'rubysauce.org'
-  }
+  config.action_mailer.default_url_options = { :host => 'rubysauce.org' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
 end
