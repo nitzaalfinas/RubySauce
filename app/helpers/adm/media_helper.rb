698ba8134db7edc74854d77bc2ef92ee239a0c1a
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-02-10 19:04:22 +0700

    fix bug

--- app/helpers/adm/media_helper.rb
+++ app/helpers/adm/media_helper.rb
@@ -1,7 +1,7 @@
 module Adm::MediaHelper
   
   def show_image(image_file_name)
-    if(File.exists?(Rails.root.to_s+'/public/images/thumb/'+image_file_name))
+    if(image_file_name && File.exists?(Rails.root.to_s+'/public/images/thumb/'+image_file_name))
       image_tag('thumb/'+image_file_name, alt: image_file_name, style: "margin-bottom: 10px; max-width: 250px;")
     else
       image_tag('thumb/default.png', style: "margin-bottom: 10px; max-width: 250px;")
